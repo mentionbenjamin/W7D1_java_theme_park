f103121f7e704d9604bb3c3e6f3dfea5b9e6a464
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-02 16:37:32 +0100

    set up dodgems constructor

--- src/main/java/Attractions/Dodgems.java
+++ src/main/java/Attractions/Dodgems.java
@@ -1,4 +1,9 @@
 package Attractions;
 
-public class Dodgems {
+public class Dodgems extends Attraction{
+
+    public Dodgems(String type){
+        this.attractions = "Dodgems";
+    }
+
 }
--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,2 +1,13 @@
+import Attractions.Dodgems;
+import org.junit.Before;
+
 public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void setUp(){
+        dodgems = new Dodgems("adrenaline");
+    }
+
 }
